b7f067a0ec7e0d3d33c2afca120edcfb2e8335f1
Sandun Chathuranga <sandunchathuranga@sanduns-macbook-air.local>
2017-05-21 18:28:10 +1000

    commit after background music

--- swift-2048/ViewController.swift
+++ swift-2048/ViewController.swift
@@ -7,35 +7,95 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var backGroundPlayer = AVAudioPlayer()
                             
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+    
+    
+    //play background music
+    
+    func playBackMusic (fileNamed: String) {
+        
+        let url = Bundle.main.url(forResource: fileNamed, withExtension: nil)
+        guard let newUrl = url else {
+            print("Couldnt find filed called \(fileNamed)")
+            return
+        }
+        
+        do {
+            backGroundPlayer = try AVAudioPlayer(contentsOf: newUrl)
+            backGroundPlayer.numberOfLoops = -1
+            backGroundPlayer.prepareToPlay()
+            backGroundPlayer.play()
+        }
+        catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
+    
 //Easy
   @IBAction func startGameButtonTapped(_ sender : UIButton) {
     let game = NumberTileGameViewController(dimension: 6, threshold: 8)
     self.present(game, animated: true, completion: nil)
+    playBackMusic(fileNamed: "back.mp3")
   }
 //Medium
     
 @IBAction func startGameButtonTapped2(_ sender: UIButton) {
     let game = NumberTileGameViewController(dimension: 5, threshold: 8)
     self.present(game, animated: true, completion: nil)
+    playBackMusic(fileNamed: "back.mp3")
     }
     
 //Hard
 @IBAction func startGameButtonTapped3(_ sender: UIButton) {
         let game = NumberTileGameViewController(dimension: 4, threshold: 8)
         self.present(game, animated: true, completion: nil)
+    playBackMusic(fileNamed: "back.mp3")
     }
     
-    @IBAction func PlayMusic(_ sender: UIButton) {
+ 
+////background
+//func playBackMusic (fileNamed: String){
+//        let url = Bundle.main.url(forResource: fileNamed, withExtension: nil)
+//        guard let newUrl = url else {
+//        
+//        print("Couldnt find filed called \(fileNamed)")
+//        return
+//        }
+//        do {
+//            backGroundPlayer = try AVAudioPlayer(contentsOf: newUrl)
+//            backGroundPlayer.numberOfLoops = -1
+//            backGroundPlayer.prepareToPlay()
+//            backGroundPlayer.play()
+//            }
+//        catch let error as NSError{
+//        print(error.description)
+//        }
+//    }
+//
+//@IBAction func PlayMusic(_ sender: UIButton) {
+//    
+//    playBackMusic(fileNamed: "back.mp3")
+//    
+//    }
     
     
+
+   
     
+    
+    @IBAction func playMusic(sender: UIButton) {
+        playBackMusic(fileNamed: "background.mp3")
     }
+
 
 
     
